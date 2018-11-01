e0e9112701d104cb4186376cf668f701e0070bcb
Antonio Xocoy Rosales <anxrosales@gmail.com>
2018-11-01 12:11:14 -0600

    feat: Login
    Cambia validaciones para realizar un Inicio de sesion correcto

--- model/usuario-model.js
+++ model/usuario-model.js
@@ -2,11 +2,24 @@ const database = require('../database/psql-connection');
 const usuario_model = {};
 
 usuario_model.login = (datos, callback) => {
-    database.query(`SELECT id, usuario FROM usuario WHERE usuario = '${datos.usuario}' AND password = '${datos.pass}'`,
+    database.query(`SELECT id, usuario, password FROM usuario WHERE usuario = '${datos.usuario}' AND password = '${datos.pass}'`,
         (error, result) => {
-            if (!!error)
+            if (!!error) {
                 callback("Error al obtener datos de la base de datos " + error, false);
-            callback(result.rows, true);
+            } else {
+                if (result.rows && result.rows.length > 0) {
+                    const usuario = result.rows[0];
+                    if (usuario.usuario == datos.usuario && usuario.password == datos.pass) {
+                        callback("Ingreso correcto", true);
+                    } else {
+                        callback("Usuario no encontrado", false);
+                    }
+                } else {
+                    callback("Usuario no encontrado", false);
+                }
+            }
+
+
         });
 };
 
